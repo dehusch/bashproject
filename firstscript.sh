7652b176f9cb2250111c4e3c47b042f155529b74
root <root@debian.debian>
2021-08-11 22:58:44 -0300

    General changes

--- firstscript.sh
+++ firstscript.sh
@@ -1,3 +1,4 @@
 echo Ola Mundo
 #echo "echo Ola mundo" > firstscript.sh
 #Line above insert a command line inside the file directly shell command
+#PATH=$PATH:diretorio Para tornar o diretorio executavel
