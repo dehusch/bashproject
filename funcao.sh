4331803bda40494b3ce552185d359bf63cbd4373
Eberhard Hüsch <32211893+dehusch@users.noreply.github.com>
2021-08-19 23:46:40 -0300

    Update Function

---
+++ funcao.sh
@@ -0,0 +1,13 @@
+#Função são pequenos programas rotineiros
+#Sintaxe:
+#nome_funcao(){comandos}
+#
+#O Programa abaixo adiciona um texto no arquivo "teste_func"
+#Pressionar enter para fechar o comando
+palavra(){
+    cd /home/d3ck
+    echo 'Linha acrescentada' >> teste_func
+}
+#
+#Para saber quais funcoes estao ativas no base:
+typeset -f
