4aa3f12db30fe708b6932e1cc1559b77937d1e08
Eberhard Hüsch <32211893+dehusch@users.noreply.github.com>
2021-08-19 22:37:33 -0300

    Update

--- firstscript.sh
+++ firstscript.sh
@@ -2,3 +2,8 @@ echo Ola Mundo
 #echo "echo Ola mundo" > firstscript.sh
 #Line above insert a command line inside the file directly shell command
 #PATH=$PATH:diretorio Para tornar o diretorio executavel
+#comando env mostra todas as variaveis do shell
+#comando ps mostra quantos shells estao abertos
+#comando "echo $" mostra na tela a variavel chamada
+#comando "teste=valor" atribui a variavel teste no shell
+#comando "echo $teste" mosdtra na tela "valor" da variavel
