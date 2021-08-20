a8c32143fcbbe778e9cd37f75bdf054b0f9e4450
Eberhard Hüsch <32211893+dehusch@users.noreply.github.com>
2021-08-19 23:15:27 -0300

    alias

---
+++ alias.sh
@@ -0,0 +1,8 @@
+#Por padrao, alias não locais ao shell
+#alias são "atalhos" para executar um ou mais comando
+#para executar um ls dentro da pasta home pode criar o atalho:
+alias lshome='cd /home; ls'
+#para executar um comando com um atributo:
+alias lsl='ls-la'
+#para excluir um alias digite:
+unalias nome_alias
