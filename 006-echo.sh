6df313f1617c098057e83318c08ea5199e38d338
dehusch <denis.huscu@gmail.com>
2021-09-02 00:26:30 -0300

    Update with erase spaces

---
+++ 006-echo.sh
@@ -0,0 +1,10 @@
+#First step is to define #!/bin/bash in the first line
+#Maybe it generate a error in interpreter, in this case, just delete it.
+#The only exception of # is not a commentary is when it put on first line
+#followed by ! character as displayed above
+
+cd / #change to / directory
+ls -la #execute ls command
+
+#After all, the permissions must be change following the command
+# chmod a+x 06 - echo.sh
