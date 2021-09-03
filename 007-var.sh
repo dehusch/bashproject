50178b431792b3ceb16ff6f19785d49e48fb3320
dehusch <denis.husch@gmail.com>
2021-09-02 23:57:58 -0300

    Exerc 7

--- 007-var.sh
+++ 007-var.sh
@@ -6,3 +6,10 @@ echo "Home DIR: $HOME"
 idade=25
 nome="Eberhard Hüsch"
 echo "O $nome tem $idade anos de idade!"
+
+dia=`date` #atribui o comando date a variavel dia
+echo "A data é $dia"
+
+#copiar a listagem de um diretorio para arquivos unicos, de acordo com a data e hora.
+arq=`date +%d%m%y%H%M`
+ls -la /home/fabio > log.$arq #apenas > sobrescreve e >> mantem e acrescenta o conteudo quando executado
