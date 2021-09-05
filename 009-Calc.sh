af7acd4bbd33d714132651b075a6ca69705a46c2
dehusch <denis.husch@gmail.com>
2021-09-05 02:52:50 -0300

    Exerc 09

---
+++ 009-Calc.sh
@@ -0,0 +1,9 @@
+#FAZENDO CALCULOS NO SHEEL
+echo $[4+3]
+#CALCULO COM VARIAVEL
+soma=$[2+7]
+echo $soma
+divid=$[$soma / 2] #aqui a variavel $soma esta sendo divida por 2
+echo $divid
+mult=$[$divid * 3]
+echo $mult
