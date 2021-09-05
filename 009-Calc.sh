a43a05e634fc144978f18175df261c1df12c0b0f
dehusch <denis.husch@gmail.com>
2021-09-05 19:38:58 -0300

    Exerc 10

--- 009-Calc.sh
+++ 009-Calc.sh
@@ -7,3 +7,17 @@ divid=$[$soma / 2] #aqui a variavel $soma esta sendo divida por 2
 echo $divid
 mult=$[$divid * 3]
 echo $mult
+#PARA CALCULOS MAIS COMPLEXOS UTILIZAR A CALC DO BASH "BC"
+#PARA ALTERAR A PRECISAO DOS NUMEROS USAR A VARIAVEL "SCALE"
+#scale=2 aparecera 2 casas decimais depois do .
+#PARA FAZER UMA CONTA DIRETO NO TERMINAL
+echo "scale=2; 2/4" | bc
+#PARA ATRIBUIR UMA VARIAVEL
+var=`echo "scale=2; 2/4" | bc` #"COLOCAR DENTRO DE "backtick ``"
+echo $var
+################################################################
+################################################################
+var1=100
+var2=30
+var3=`echo "scale=3; $var1/$var2" | bc`
+echo $var3
