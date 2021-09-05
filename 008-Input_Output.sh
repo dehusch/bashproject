508a5844ad35753a400287e98aa5d5966c559670
dehusch <denis.husch@gmail.com>
2021-09-05 02:14:44 -0300

    Exerc 09

--- 008-Input_Output.sh
+++ 008-Input_Output.sh
@@ -1,2 +1,8 @@
 #Output
 #Enviar a saida de um comando para um arquivo
+date > file.txt
+#Sera gerado um arquivo.txt e gavrado a data nele.
+wc < file.txt
+#WC conta quantas linhas, palavras e caracteres tem o texto
+#<< abre a possibilidade de adicionar varias linhas para ser executado no comando wc
+wc << texto
